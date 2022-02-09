3a112453b72d6a1476767ca761d02fbfc27ada35
Diksha8511 <diksha85119@gmail.com>
2022-02-09 12:16:42 +0530

    select by actor name functionality added

--- crud_operations/fetch_by_actor.py
+++ crud_operations/fetch_by_actor.py
@@ -3,9 +3,11 @@ import sqlite3  #necessary packages imported
 connection = sqlite3.connect('../movies.db')   # it creates the database
 cursor = connection.cursor()
 
-# insert query
-insertCommand = "INSERT INTO details VALUES ('Noteok', 'Zaheer bal', 'Pranutan Bahl', 'Nitin Kakkar', 2019)"
+#select query
+selectCommand = "SELECT * FROM details where actor = 'Zaheer Iqbal'"
 
 # execute method fires the query that we pass as a parameter
-cursor.execute(insertCommand)
-connection.commit()
+cursor.execute(selectCommand)
+
+movies = cursor.fetchall()
+print(movies)
