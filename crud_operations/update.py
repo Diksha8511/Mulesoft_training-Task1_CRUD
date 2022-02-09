b31521daee8e1197a6ea6694888e544f21fdaec4
Diksha8511 <diksha85119@gmail.com>
2022-02-09 12:05:40 +0530

    update query created

--- crud_operations/update.py
+++ crud_operations/update.py
@@ -2,3 +2,6 @@ import sqlite3  #necessary packages imported
 
 connection = sqlite3.connect('../movies.db')   # it creates the database
 cursor = connection.cursor()
+
+#update query
+updateCommand = "UPDATE details SET name = 'test'"
