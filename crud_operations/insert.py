6bfecbc265517177b0a8e46652fc9d979514b190
Diksha8511 <diksha85119@gmail.com>
2022-02-09 11:56:51 +0530

    insert functionality completed and test successfully

--- crud_operations/insert.py
+++ crud_operations/insert.py
@@ -1,6 +1,11 @@
 import sqlite3  #necessary packages imported
 
-connection = sqlite3.connect('movies.db')   # it creates the database
+connection = sqlite3.connect('../movies.db')   # it creates the database
 cursor = connection.cursor()
 
-insertCommand = "INSERT INTO details VALUES ('Notebook', 'Zaheer Iqbal', 'Pranutan Bahl', 'Nitin Kakkar', 2019)"
+# insert query
+insertCommand = "INSERT INTO details VALUES ('Noteok', 'Zaheer Iqbal', 'Pranutan Bahl', 'Nitin Kakkar', 2019)"
+
+# execute method fires the query that we pass as a parameter
+cursor.execute(insertCommand)
+connection.commit()
