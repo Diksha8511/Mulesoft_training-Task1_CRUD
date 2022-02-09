87e8e4920d0453cd7c43e68ba6d1cb754a64f1de
Diksha8511 <diksha85119@gmail.com>
2022-02-09 11:40:15 +0530

    create table query created

--- createTable.py
+++ createTable.py
@@ -2,3 +2,6 @@ import sqlite3  #necessary packages imported
 
 connection = sqlite3.connect('movies.db')   # it creates the database
 cursor = connection.cursor()
+
+# query to create table
+createTableCommand = "CREATE TABLE IF NOT EXISTS details (name TEXT, actor TEXT, actress TEXT, director TEXT, yearOfRelease INTEGER)"
